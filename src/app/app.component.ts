ed45be7cf9c0de5a6802937951e246a23f8306f2
Michael Walker <walkerrepair255@yahoo.com>
2021-11-15 16:23:34 -0500

    first agular dabblings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'My';
+
+  update(msg: string): void {
+    this.title = `${msg}!!!`
+  }
 }
